c75542f6badb060bdde53d6511b54b6e0e0ef3ea
Alexandr Pivovarov <113209298+pvvrv@users.noreply.github.com>
2023-02-20 19:35:48 +0300

    Add files via upload

---
+++ ПР 2 задание 3/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include "Figure.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	vector<Figure> mas;
+	float x1, x2, x3, x4, y1, y2, y3, y4;
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Введите координаты x1,x2,x3,x4,y1,y2,y3,y4 для четырехугольника №" << i + 1 << endl;
+		cin >> x1 >> x2 >> x3 >> x4 >> y1 >> y2 >> y3 >> y4;
+		mas.push_back(Figure(x1, x2, x3, x4, y1, y2, y3, y4));
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Параметры четырехугольника № " << i + 1 << " : "; mas[i].show();
+		cout << "Является ли прямоугольником четырехугольник  № " << i + 1 << " : " << mas[i].is_prug() << endl;
+		cout << "Является ли квадратом четырехугольник  № " << i + 1 << " : " << mas[i].is_square() << endl;
+		cout << "Является ли ромбом четырехугольник  № " << i + 1 << " : " << mas[i].is_romb() << endl;
+		cout << "Можно ли вписать окружность в четырехугольник  № " << i + 1 << " : " << mas[i].is_in_circle() << endl;
+		cout << "Можно ли описать четырехугольник  № " << i + 1 << " : " << mas[i].is_out_circle() << endl;
+	}
+}
