074397086b8a3b6931fae62fb85f3d07b3231395
Alexandr Pivovarov <113209298+pvvrv@users.noreply.github.com>
2023-05-28 22:59:26 +0300

    Add files via upload

---
+++ Практика 7/Задание 1 ПР9/pr9_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "swap.h" 
+
+struct Point {
+    int x, y;
+};
+
+void printPoint(Point p) {
+    std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
+}
+
+int main() {
+    Point a = { 1, 2 };
+    Point b = { 3, 4 };
+
+    std::cout << "Before exchange:" << std::endl;
+    printPoint(a);
+    printPoint(b);
+
+    swap(a, b);
+
+    std::cout << "After exchange:" << std::endl;
+    printPoint(a);
+    printPoint(b);
+
+    return 0;
+}
